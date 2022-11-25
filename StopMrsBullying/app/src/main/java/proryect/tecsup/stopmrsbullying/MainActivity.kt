0e7385b0602c735ff7bdfbea1d3972dc6bb23885
CristianBlaz18 <danielblaz59@gmail.com>
2022-11-24 20:39:02 -0500

    Subida de proyecto

---
+++ StopMrsBullying/app/src/main/java/proryect/tecsup/stopmrsbullying/MainActivity.kt
@@ -0,0 +1,16 @@
+package proryect.tecsup.stopmrsbullying
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val accesToken ="";
+    val REQUEST = 200;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun configAsistente(){
+
+    }
+}
